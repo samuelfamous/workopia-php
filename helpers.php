247ba8272f3eed9df54213e5eae8ce1a63c45649
samuel famous <samuelfamous22@yahoo.com>
2024-06-11 13:45:27 -0400

    Initial Commit and started home view

---
+++ helpers.php
@@ -0,0 +1,10 @@
+ <?php
+    /**
+     * Get the path
+     * @param string $path
+     * @return string
+     */
+    function basePath($path = '')
+    {
+        return __DIR__ . '/' . $path;
+    }
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require '../views/home.view.php';
+require '../helpers.php';
+require basePath('views/home.view.php');
